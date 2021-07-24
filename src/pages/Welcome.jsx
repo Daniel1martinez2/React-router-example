9333897ae2fd1606aef3de0e439dc6e3af861bb4
Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>
2021-07-24 15:26:35 -0500

    Reirect from other path
    Signed-off-by: Daniel1martinez2 <daniel.martinezvillegas2001@gmail.com>

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,6 +1,12 @@
+import { Route } from 'react-router-dom';
 const Welcome = () => {
   return (
-    <h1>Welcome Page</h1>
+    <section>
+      <h1>Welcome Page</h1>
+      <Route path="/welcome/new-user">
+        <p>Hi, New User 😎👍</p>
+      </Route>
+    </section>
   )
 };
 export default Welcome; 
